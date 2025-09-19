1b05c7e8058efbbf812595b2264a588c51cea858
davidmaykon22-hub <david.maykon22@gmail.com>
2025-09-19 01:42:37 +0000

    incluso bilbioteca e estrutura de itens

---
+++ freefire novato.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_ITENS 10
+
+// =====================================
+//Estrutura que representa um item na mochila
+//======================================
+
+typedef struct {
+    char [30];         // Nome do item
+    char tipo[20];     //Tipo do item ex
+    int quantidade;    // Quantidade de itens
+
+} Item;
