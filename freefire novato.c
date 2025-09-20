517658426fbae1ba6526ba6316f8e145d63a49fe
davidmaykon22-hub <david.maykon22@gmail.com>
2025-09-20 04:27:28 +0000

    inserido swirch case

--- freefire novato.c
+++ freefire novato.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define MAX_ITENS 10
 
@@ -9,12 +10,32 @@
 //======================================
 
 typedef struct {
-    char [30];         // Nome do item
+    char nome[30];         // Nome do item
     char tipo[20];     //Tipo do item ex
     int quantidade;    // Quantidade de itens
+    int prioridade;
 
 } Item;
 
+// Funções do sistema
+void inserirItem(Item mochila[], int *contador);
+void listarItens(Item mochila[], int contador);
+void removerItem(Item mochila[], int *contador);
+void buscarItem(Item mochila[], int contador);
+void pausar();
+
 // Vetor global que armazena os itens
 Item mochila[MAX_ITENS];
-int qtdItens = 0; // Quantidade atual de itens cadastrados
+int qtdItens = 0; // Quantidade atual de itens na mochila
+int opção;
+
+do {
+        printf("\n=== MENU INVENTÁRIO ===\n");
+        printf("1. Adicionar item\n");
+        printf("2. Remover item\n");
+        printf("3. Listar itens\n");
+        printf("4. Buscar item\n");
+        printf("0. Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+        getchar(); // Consumir o '\n' deixado pelo scanf
